fc6a59ed51fa2e2f996428b2eec6614c48a43794
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-22 22:42:26 -0300

    feat: Adicionando o model Agendamentos em agendamentos/models.py

--- question_3/agendamentos/models.py
+++ question_3/agendamentos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Agendamentos(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=13)
+    data = models.DateTimeField()
+    cancelamento = models.BooleanField(default=False)
