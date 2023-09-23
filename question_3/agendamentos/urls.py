174de385eece9cf5bd0d66c6d71f794820097ef3
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-22 23:00:45 -0300

    feat: Adicionando a rota "agendamento/<int:pk>/" em agendamentos/urls.py

--- question_3/agendamentos/urls.py
+++ question_3/agendamentos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from agendamentos.views import AgendamentosList
+from agendamentos.views import AgendamentosList, AgendamentoEspecifico
 
 urlpatterns = [
     path('agendamentos/', AgendamentosList.as_view()),
+    path('agendamento/<int:pk>/', AgendamentoEspecifico.as_view())
 ]
