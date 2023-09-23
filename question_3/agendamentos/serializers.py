b56376d2b0ef838ff6e1947f216962dffeddbe52
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-23 09:50:05 -0300

    feat: Adicionando a validação "validate_data" em agendamentos/serializers.py

--- question_3/agendamentos/serializers.py
+++ question_3/agendamentos/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
+from django.utils import timezone
 from agendamentos.models import Agendamentos
+from rest_framework import serializers, status
+from agendamentos.utils import get_horario_disponiveis
 
 
 class AgendamentosSerializer(serializers.ModelSerializer):
@@ -12,3 +14,17 @@ class AgendamentosSerializer(serializers.ModelSerializer):
             'data',
             'cancelamento',
         ]
+
+    def validate_data(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError(
+                detail='O agendamento não pode ser feito no passado.',
+                code=status.HTTP_400_BAD_REQUEST
+            )
+
+        if value not in get_horario_disponiveis(value.date()):
+            raise serializers.ValidationError(
+                'Esse horário não está disponível.'
+            )
+
+        return value
