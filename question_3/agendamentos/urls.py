8529dbf07f9c174c9a9d944c6b22e40ea93719c4
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-23 00:04:40 -0300

    feat: Adicionando 4 endpoints em agendamentos/urls.py
     - Endpoint "cadastrar_agendamento/" para cadastrar novos agendamentos.
     - Endpoint "cancelar_e_editar_agendamento/<int:pk>/" para editar ou cancelar um agendamento.
     - Endpoint "listar_agendamentos/" para listar todos os agendamentos.
     - Endpoint "agendamento/<int:pk>" para listar um agendamento específico.

--- question_3/agendamentos/urls.py
+++ question_3/agendamentos/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from agendamentos.views import AgendamentosList, AgendamentoEspecifico
+from agendamentos.views import (
+    cancelar_e_editar,
+    ListarAgendamentos,
+    CadastrarAgendamento,
+    AgendamentoEspecifico,
+)
 
 urlpatterns = [
-    path('agendamentos/', AgendamentosList.as_view()),
-    path('agendamento/<int:pk>/', AgendamentoEspecifico.as_view())
+    path('cadastrar_agendamento/', CadastrarAgendamento.as_view()),
+    path('cancelar_e_editar_agendamento/<int:pk>/', cancelar_e_editar),
+    path('listar_agendamentos/', ListarAgendamentos.as_view()),
+    path('agendamento/', AgendamentoEspecifico.as_view())
 ]
